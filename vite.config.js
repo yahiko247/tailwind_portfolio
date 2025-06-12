d8059a5b736f88fd52c036a2a81594fddd922bdc
vincent <vincegams24@gmail.com>
2025-06-11 21:49:03 -0700

    update_button and add configuration to expose to local network

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Expose the server to your local network
+    port: 5173        // Optional, but can set if you want to fix the port
+  }
 })
