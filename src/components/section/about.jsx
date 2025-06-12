479fb777ff95f7c6f5046cad252d4040fb42f33c
vincent <vincegams24@gmail.com>
2025-06-11 22:02:12 -0700

    update_to_ add_ design to home and button of contact

---
+++ src/components/section/about.jsx
@@ -0,0 +1,10 @@
+export const About = () => {
+    return (
+        <section
+            id="about"
+            className="min-h-screen flex items-center justify-center py-20"
+        >
+
+        </section>
+    )
+}
--- src/components/section/home.jsx
+++ src/components/section/home.jsx
@@ -10,7 +10,8 @@ export const Home = () => {
                     👋 Hi, I’m Yahiko, a fourth-year student from USTP. 👀 I’m interested in both IoT and web/app development. 🌱 I’m currently learning how to integrate machine learning into my projects. 💞️ I’m collaborating with my capstone buddy. 📫 You can reach me via Facebook/Instagram. ⚡ Fun fact: Don’t expect too much from me, AHAHAHA!
                 </p>
                 <div className=" flex justify-center space-x-4">
-                    <a href="#projects" className="bg-blue-500 text-white py-3 px-3 rounded font-medium transition relative overflow-hidden hover:-tanslate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246,0.4)]">View Projects</a>
+                    <a href="#projects" className="bg-blue-500 text-white py-3 px-3 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">View Projects</a>
+                    <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">Contact Me</a>
                 </div>
             </div>
 
