8089e2166dbcf87106a46a19ae7452ba2e1cc426
vincent <vincegams24@gmail.com>
2025-06-12 16:13:45 -0700

    update add about info skills backen and frontend

--- src/components/section/about.jsx
+++ src/components/section/about.jsx
@@ -1,4 +1,7 @@
 export const About = () => {
+
+    const frontendSkill = ["React", "TailwindCss", "Flutter", "MUI"]
+    const backendSkill = ["Nextjs", "Python", "firebase"]
     return (
         <section
             id="about"
@@ -17,6 +20,35 @@ export const About = () => {
                         projects to build innovative and sustainable technologies.
 
                     </p>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {frontendSkill.map((tech, key) => (
+                                <span 
+                                    key={key} 
+                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                             hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                                {tech}
+
+                                             </span>))}
+                            </div>
+                        </div>
+                        
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {backendSkill.map((tech, key) => (
+                                    <span 
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)]" transition
+                                    >{tech}</span>
+                                ))}
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
 
